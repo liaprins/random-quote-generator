aaf56cb46112e17d40b8a88b0eaad8658fb81785
Lia Prins <liajprins@gmail.com>
2023-03-20 14:10:34 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import * as React from "react"; // import everything
+import { Fragment } from "react"; // name these imports
+import { useState } from 'react';
+
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, 🌎!</h1>
+    </div>
+  );
+}
+<HelloWorld />
